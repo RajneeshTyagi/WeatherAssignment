8b04bd21571538060d52a4724cc3dd32590aac18
Rajneesh Tyagi <rajneesht542@gmail.com>
2020-04-06 00:26:07 +0530

    inintal Rep Done

--- app/src/main/java/com/rajneesh/assignment/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/rajneesh/assignment/view/fragment/HomeFragment.kt
@@ -1,7 +1,11 @@
 package com.rajneesh.assignment.view.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.rajneesh.assignment.R
@@ -14,6 +18,21 @@ class HomeFragment : BaseFragment() {
     override fun parentView() = parentLayout
 
     lateinit var mWhetherViewModel: WhetherViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val listItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.fragment_home,
+            container,
+            false
+        ) as ViewDataBinding
+
+        return listItemBinding.root
+
+    }
 
     override fun init(view: View, savedInstanceState: Bundle?) {
         mWhetherViewModel = ViewModelProviders.of(this).get(WhetherViewModel::class.java)
--- app/src/main/java/com/rajneesh/assignment/view/fragment/SplashFragment.kt
+++ app/src/main/java/com/rajneesh/assignment/view/fragment/SplashFragment.kt
@@ -2,7 +2,11 @@ package com.rajneesh.assignment.view.fragment
 
 import android.os.Bundle
 import android.os.Handler
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.rajneesh.assignment.R
@@ -11,13 +15,25 @@ import kotlinx.android.synthetic.main.fragment_splash.*
 class SplashFragment : BaseFragment() {
     lateinit var mHomeNavigation: NavController
     override fun parentView() = parentLayout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val listItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.fragment_splash,
+            container,
+            false
+        ) as ViewDataBinding
+
+        return listItemBinding.root
+
+    }
 
     override fun init(view: View, savedInstanceState: Bundle?) {
         mHomeNavigation = Navigation.findNavController(view)
         Handler().postDelayed({
-            //            LoginFragment.start(this)
-//            HomeActivity.start(this)
-//            finish()
             mHomeNavigation.navigate(R.id.action_splashFragment_to_homeFragment)
         }, 2000)
     }
